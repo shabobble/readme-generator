c140ef5580cfd37d84479d2be8e9aaa7a516442b
shabobble <pksullivan@yahoo.com>
2021-05-02 17:02:51 -0400

    added list of inquirer prompt questions

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,54 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your project title?',
+        name: 'projectName',
+    },
+    {
+        type: 'input',
+        message: 'Please provide a short description of your project.',
+        name: 'projectDescription'
+    },
+    {
+        type: 'input',
+        message: 'Please provide any installation instructions for your project',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Please provide any usage information for your project.',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Who else contributed to this project?',
+        name: 'contributors',
+    },
+    {
+        type: 'input',
+        message: 'Please provide testing instructions.',
+        name: 'testing',
+    },
+    {
+        type: 'list',
+        message: 'Please choose a license.',
+        choices: ['MIT License', 'The Unlicense', 'Mozilla Public License 2.0'],
+        name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'What is your Github username?',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'What is your e-mail address?',
+        name: 'email',
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
